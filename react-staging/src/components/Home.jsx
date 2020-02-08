6c7521537ddb84192e799cf6bf399e7add7d2e2f
houzhongqin <614977099@qq.com>
2020-02-08 20:08:54 +0800

    20200208

--- react-staging/src/components/Home.jsx
+++ react-staging/src/components/Home.jsx
@@ -4,7 +4,7 @@ export default class Home extends Component {
     render() {
         return (
             <div className="panel-body">
-                <h3>我是Home的内容</h3>
+                <h3>我是Home的内容!</h3>
             </div>   
         )
     }
